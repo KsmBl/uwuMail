3e28c2091927c80d88564ebebd03cedc27d4a4a1
KsmBl <katzen.sind.lecker69@gmail.com>
2026-09-07 13:09:58 +0200

    Release 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "de.uwumail"
         minSdk = 31
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         vectorDrawables { useSupportLibrary = true }
 
         buildConfigField(
